c800453c54df7355584c282517a27d3def418d19
Abdrasul <abdrasulmihalko@gmail.com>
2022-05-24 01:11:18 +0600

    feat: added rendering html

--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import "github.com/gin-gonic/gin"
 func main() {
 	router := gin.Default()
 
-	router.GET("/check", func(context *gin.Context) {
-		context.String(200, "%s", "Hello, world!")
+	router.LoadHTMLGlob("assets/*")
+
+	router.GET("/upload", func(context *gin.Context) {
+		context.HTML(200, "uploader.html", gin.H{})
 	})
 
 	router.Run(":8080")
